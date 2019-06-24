b606f89839228e7b832c6ea66a2ece7014f8ea62
unknown <18507040452@163.com>
2019-06-24 09:47:18 +0800

    entry

--- 修仙中/练气期/Js/unique.js
+++ 修仙中/练气期/Js/unique.js
@@ -1,14 +1,14 @@
 let arr = '2223334314aabssbsadndmfsdfsfad1122'.split("")
-function unique(arr) {
-    let newarr = []
-    arr.forEach((item, index) => {
+// function unique(arr) {
+//     let newarr = []
+//     arr.forEach((item, index) => {
 
-        if (!newarr.includes(item)) {
-            newarr.push(item)
-        }
-    });
-    return newarr
-}
+//         if (!newarr.includes(item)) {
+//             newarr.push(item)
+//         }
+//     });
+//     return newarr
+// }
 
 
 // function unique(arr) {
@@ -16,7 +16,26 @@ function unique(arr) {
 //     arr.forEach((item, index) => {
 //         if (newarr.indexOf(item) === -1) {
 //             newarr.push(item)
+//             console.log(newarr)
 //         }
 //     });
 //     return newarr
 // }
+
+
+
+function unique(arr) {
+
+    let arr1 = []
+    let obj1 = {}
+    for (var i = 0; i < arr.length; i++) {
+
+        if (!obj1[arr[i]]) {
+            arr1.push(arr[i])
+            obj1[arr[i]] = 1
+            console.log(arr1)
+        }
+    }
+    return arr1
+}
+unique(arr)
